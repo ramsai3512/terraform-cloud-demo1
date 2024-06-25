c6316d06506c5224794833a5b47251b2b383ae2f
ramsai3512 <142394073+ramsai3512@users.noreply.github.com>
2024-06-25 07:02:49 +0530

    Update c4-ec2-instacne.tf

--- c4-ec2-instacne.tf
+++ c4-ec2-instacne.tf
@@ -1,5 +1,5 @@
 # Create EC2 Instance - Amazon2 Linux
-resource "aws_instance" "my-ec2" {
+resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
   count = 1
